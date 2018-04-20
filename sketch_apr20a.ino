283e787f88e59476eef42498c2d2c31ad0175efb
dlimasp <38563512+dlimasp@users.noreply.github.com>
2018-04-20 14:07:09 -0600

    Add files via upload
    Programa de prueba servomotor con LEDs

---
+++ sketch_apr20a.ino
@@ -0,0 +1,40 @@
+#include <Servo.h>
+Servo servomotor;
+char opened;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  servomotor.attach(9);
+  opened.attach(7);
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(7, INPUT);
+  
+  
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+if (Serial.available()>0){
+  delay(20);
+opened = Serial.read();
+
+if( opened == 1)
+{
+servomotor.write(90);
+digitalWrite(2, HIGH);
+digitalWrite(4, LOW);
+}
+
+else if ( opened == 0)
+{
+  servomotor.write(180);
+  digitalWrite(2, LOW);
+  digitalWrite(4, HIGH);
+}
+
+}
+}
